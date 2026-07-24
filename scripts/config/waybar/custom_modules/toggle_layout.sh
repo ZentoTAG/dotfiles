02fc8b4445415c788d27d6bfcc3292f3ba2ef6b3
ZentoTag <tagvad2007@gmail.com>
2026-07-25 03:14:49 +0500

    clean dotfiles

--- scripts/config/waybar/custom_modules/toggle_layout.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-hyprctl dispatch switchxkblayout 0 next 2>/dev/null || hyprctl dispatch switchxkblayout 1 next 2>/dev/null
--- scripts/config/waybar/scripts/wifi.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Список сетей через wofi
-networks=$(nmcli -t -f SSID,SIGNAL dev wifi list | awk -F':' '{print $1 " (" $2 "%)"}' | wofi --dmenu -p "  Wi-Fi" -i -lines 10 -width 300)
-
-if [ -n "$networks" ]; then
-    ssid=$(echo "$networks" | awk -F' (' '{print $1}')
-    nmcli dev wifi connect "$ssid"
-fi
