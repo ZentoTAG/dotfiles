f9eb04175ba996af3eecdad6c3418835eaf4f20f
ZentoTag <tagvad2007@gmail.com>
2026-07-26 04:13:38 +0500

    added swayins, deleted dunst

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,10 +4,14 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 function fish_greeting
 end
 
+# Копирование в буфер
 function copy
     wl-copy
 end
 
+# ============================================
+# РЕДАКТОРЫ И РАЗРАБОТКА
+# ============================================
 alias b "bash"
 alias m "micro"
 alias g "git"
@@ -16,18 +20,38 @@ alias ga "git add"
 alias gc "git commit -m"
 alias gpush "git push"
 alias gpull "git pull"
+
+# ============================================
+# ПРОЕКТЫ И КОНФИГИ
+# ============================================
 alias prog "cd ~/data/programs/"
+alias dots "cd ~/data/dotfiles"
 alias com "micro ~/data/dotfiles/docs/commands.md"
 alias combat "bat ~/data/dotfiles/docs/commands.md"
 alias comcat "cat ~/data/dotfiles/docs/commands.md"
-alias dots "cd ~/data/dotfiles"
+alias hypr "micro ~/.config/hypr/hyprland.conf"
+alias config "cd ~/.config/"
+
+# ============================================
+# СИСТЕМА
+# ============================================
 alias upd "sudo pacman -Syu"
 alias kl "killall"
+alias vacuum "journalctl --vacuum-size=100M"
+alias syslog_emerg "sudo dmesg --level=emerg,alert,crit"
+alias syslog "sudo dmesg --level=err,warn"
+
+# ============================================
+# УТИЛИТЫ
+# ============================================
+alias waybar "pkill waybar && nohup waybar > /dev/null 2>&1 &"
 alias yt1080 "yt-dlp -f 'bestvideo[height<=1080]+bestaudio/best'"
 alias cmatrix "cmatrix -C white"
 alias v2rayn "~/.data/programs/v2rayN-linux-64/v2rayN"
-alias hypr "micro ~/.config/hypr/hyprland.conf"
 
+# ============================================
+# САЙТЫ
+# ============================================
 alias gh "xdg-open https://github.com"
 alias yt "xdg-open https://youtube.com"
 alias gmail "xdg-open https://mail.google.com"
@@ -43,18 +67,23 @@ alias r34 "xdg-open https://rule34hub.com/"
 alias translate "xdg-open https://translate.yandex.ru/"
 alias gpt "xdg-open https://chatgpt.com/"
 
-alias syslog_emerg "sudo dmesg --level=emerg,alert,crit"
-alias syslog "sudo dmesg --level=err,warn"
-alias vacuum "journalctl --vacuum-size=100M"
-
+# ============================================
+# ПЕРЕМЕННЫЕ ОКРУЖЕНИЯ
+# ============================================
 set -gx EDITOR micro
 set -gx VISUAL micro
 set -gx BROWSER zen-browser
 set -gx PATH $PATH ~/.local/bin
+set -gx PATH $PATH ~/data/dotfiles/scripts
 
-# bun
+# Bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+
+
+# ============================================
+# АВТОЗАПУСК HYPRLAND
+# ============================================
 if status is-login && test -z "$WAYLAND_DISPLAY" && test (tty) = /dev/tty1
     exec start-hyprland
 end
