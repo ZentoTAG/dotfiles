41d42f7b2fb0f0f0bba54816d0fce8f3cb650a2c
ZentoTag <tagvad2007@gmail.com>
2026-08-27 18:47:22 +0500

    update

--- config/hypr/lua/monitors.lua
+++ config/hypr/lua/monitors.lua
@@ -1,7 +1,16 @@
+-- Второй монитор (HDMI-A-2) — СЛЕВА
+hl.monitor({
+    output = "HDMI-A-2",
+    mode = "1366x768@59.79",
+    position = "0x0",
+    scale = 1,
+})
+
+-- Основной монитор (DP-1) — СПРАВА от HDMI
 hl.monitor({
     output = "DP-1",
     mode = "1920x1080@180",
-    position = "0x0",
+    position = "1366x0",
     scale = 1,
 })
 hl.monitor({
